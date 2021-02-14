cdf643e0ddd5a0d61a096704920be173a57b4fae
ignatiy-nikitin <ignat.no@yandex.ru>
2021-02-14 21:57:29 +0300

    fix 2

--- carts/serializers.py
+++ carts/serializers.py
@@ -41,7 +41,6 @@ class CartItemSerializer(serializers.ModelSerializer):
     def validate(self, attrs):
         if self.instance:
             if not self.context['request'].user.cart.cart_items.filter(id=self.instance.id).exists():
-            # if self.instance.id not in [cart_item.id for cart_item in self.context['request'].user.cart.cart_items.all()]:
                 raise ValidationError('Сan not change the cart item that exists in the order!')
         return attrs
 
